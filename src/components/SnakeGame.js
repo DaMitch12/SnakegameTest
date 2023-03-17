7309e559e3d502085865ce920ebbe07018cecf20
DaMitch12 <90734634+DaMitch12@users.noreply.github.com>
2023-03-16 20:13:22 -0400

    Create SnakeGame.js

---
+++ src/components/SnakeGame.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class SnakeGame extends Component {
+  // Implement your Snake game component here.
+  // You can use a library like react-game-kit, or create your custom Snake game component.
+}
+
+export default SnakeGame;
